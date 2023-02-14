a74cec95c44f92bd4419f6cd0e158340e86e460e
Inna Kokorina <inna_kokorina@bk.ru>
2023-02-14 12:29:17 +0400

    add second VC

---
+++ App/UI/Main/SecondViewContrtoller.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewContrtoller.swift
+//  App
+//
+//  Created by Inna Kokorina on 14.02.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
